272883daf2b01eef351af985718b75e40553e033
Mykenzie Rogers <mykenzierogers@Mykenzies-MacBook-Pro.local>
2019-01-30 15:45:34 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
+            className="App-linfk"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
